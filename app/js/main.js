fab1d82623c7a177cb6f934195666a6e8a1f9ff5
Ólafur Örn Nielsen <olafur@form5.is>
2013-11-09 14:34:36 +0000

    Configure ng-view

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,39 @@
-require.config({
-  baseUrl: 'js',
-  paths: {
-    modernizr: 'vendor/modernizr',
-    jQuery: 'vendor/jquery',
-    angular: 'vendor/angular',
-    ngResource: 'vendor/angular-resource',
-    ngRoute: 'vendor/angular-route',
-    ngAnimate: 'vendor/angular-animate'
-  },
-  shim: {
-    angular: {
-      exports: 'angular'
+(function() {
+  require.config({
+    baseUrl: 'js',
+    paths: {
+      modernizr: 'vendor/modernizr',
+      jQuery: 'vendor/jquery',
+      angular: 'vendor/angular',
+      ngResource: 'vendor/angular-resource',
+      ngRoute: 'vendor/angular-route',
+      ngAnimate: 'vendor/angular-animate'
     },
-    ngResource: {
-      deps: ['angular']
-    },
-    ngRoute: {
-      deps: ['angular']
-    },
-    ngAnimate: {
-      deps: ['angular']
-    },
-    jQuery: {
-      exports: 'jQuery'
-    },
-    Modernizr: {
-      exports: 'Modernizr'
+    shim: {
+      angular: {
+        exports: 'angular'
+      },
+      ngResource: {
+        deps: ['angular']
+      },
+      ngRoute: {
+        deps: ['angular']
+      },
+      ngAnimate: {
+        deps: ['angular']
+      },
+      jQuery: {
+        exports: 'jQuery'
+      },
+      Modernizr: {
+        exports: 'Modernizr'
+      }
     }
-  }
-});
-
-require(['jQuery', 'angular', 'config/routes'], function($, angular) {
-  $(function() {
-    angular.bootstrap(document, ['app']);
   });
-});
+
+  require(['jQuery', 'angular', 'config/routes'], function($, angular) {
+    $(function() {
+      angular.bootstrap(document, ['app']);
+    });
+  });
+}).call(this);
