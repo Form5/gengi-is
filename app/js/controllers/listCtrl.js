6248ade070e9fac4857f3b86dc3bacb7aa1ed4de
Ólafur Örn Nielsen <olafur@form5.is>
2013-11-09 17:57:09 +0000

    Moving everything under ng-view

--- app/js/controllers/listCtrl.js
+++ app/js/controllers/listCtrl.js
@@ -3,6 +3,9 @@
     return app.controller('listCtrl', function($rootScope, $scope, Currency) {
       $rootScope.pageTitle = 'Gengi.is';
       $scope.items = Currency.query();
+      $scope.updateView = function(type) {
+        $scope.listView = type;
+      };
     });
   });
 }).call(this);
