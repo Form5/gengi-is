af02ff7cd99eafbdd13c9c82cb52233ce2ba5eab
Ólafur Örn Nielsen <olafur@form5.is>
2013-11-10 00:02:13 +0000

    Pre-parse user input and round result before showing

--- app/js/controllers/calcCtrl.js
+++ app/js/controllers/calcCtrl.js
@@ -10,6 +10,9 @@
             function sucess(curr) {
               $scope.currency = curr;
 
+              $scope.fromValue = 1;
+              $scope.calcRate = calcRate;
+
               // Header
               $rootScope.pageTitle = curr.longName + ' - Gengi.is';
             },
@@ -21,4 +24,15 @@
       );
     }
   );
+
+  function calcRate(rate, amount) {
+    rate = parseFloat(String(rate).replace(',', '.'), 0);
+    amount = parseFloat(String(amount).replace(',', '.'), 0);
+
+    var value = (rate*amount);
+
+    if(isNaN(value)) return 0;
+    return value.toFixed(1);
+  }
+
 }).call(this);
