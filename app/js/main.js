a3d5fc98ccb946ae7b04f76243cd9819afaa3935
Ólafur Örn Nielsen <olafur@form5.is>
2013-11-11 22:57:48 +0000

    Remove jQuery as dependency and use .min version of js libraries

--- app/js/main.js
+++ app/js/main.js
@@ -4,11 +4,10 @@
     paths: {
       text: 'vendor/text',
       modernizr: 'vendor/modernizr',
-      jQuery: 'vendor/jquery',
-      angular: 'vendor/angular',
-      ngResource: 'vendor/angular-resource',
-      ngRoute: 'vendor/angular-route',
-      ngAnimate: 'vendor/angular-animate',
+      angular: 'vendor/angular.min',
+      ngResource: 'vendor/angular-resource.min',
+      ngRoute: 'vendor/angular-route.min',
+      ngAnimate: 'vendor/angular-animate.min',
       underscore: 'vendor/underscore'
     },
     shim: {
@@ -22,9 +21,20 @@
     }
   });
 
-  require(['jQuery', 'angular', 'config/routes'], function($, angular) {
-    $(function() {
+  require(['angular', 'config/routes'], function(angular) {
+
+    var loaded = 0;
+    var bootstrap = function() {
+      if (loaded) return;
+      loaded = 1;
       angular.bootstrap(document, ['app']);
-    });
+    };
+
+    if ( document.readyState === 'complete' ) {
+      setTimeout( bootstrap );
+    } else {
+      document.addEventListener( 'DOMContentLoaded', bootstrap, false );
+      window.addEventListener( 'load', bootstrap, false );
+    }
   });
 }).call(this);
