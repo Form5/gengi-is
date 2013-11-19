17d53377a72237a2536b865fcbcd3609cdf33f7c
Benedikt D. Valdez Stefánsson <benediktvaldez@gmail.com>
2013-11-19 22:18:18 +0000

    Save listView and listExpand status in localStorage

--- app/js/controllers/listCtrl.js
+++ app/js/controllers/listCtrl.js
@@ -5,9 +5,24 @@
 
       // Header
       $rootScope.headerTpl = 'partials/listHeader.html';
-      $rootScope.listView = 'list';
+
+      if (localStorage.getItem('listView') === null) {
+        localStorage.setItem('listView', 'list');
+      }
+      $rootScope.listView = localStorage.getItem('listView');
+
+      if (localStorage.getItem('listExpand') === null) {
+        localStorage.setItem('listExpand', '');
+      }
+      $rootScope.listExpand = localStorage.getItem('listExpand');
+
       $rootScope.updateView = function(type) {
         $rootScope.listView = type;
+        localStorage.setItem('listView', type);
+      };
+      $rootScope.expandList = function(value) {
+        $rootScope.listExpand = value;
+        localStorage.setItem('listExpand', value);
       };
     });
   });
